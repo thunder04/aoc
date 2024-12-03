80bb511a3be44c14a2711fe12d39d48a4cf4aa41
thunder04 <70254851+thunder04@users.noreply.github.com>
2024-12-03 23:25:09 +0200

    Add 2024, day 3, p. 1

--- solutions/src/_2024/three/mod.rs
+++ solutions/src/_2024/three/mod.rs
@@ -1,12 +1,50 @@
+use memchr::memmem::Finder;
+
 static INPUT: &[u8] = include_bytes!("./input.txt");
 
 pub fn run() -> super::Runner {
     (Some(part_1), None)
 }
 
-// Answer: ???
+macro_rules! lazy_read_number {
+    ($idx: expr, $off: expr, $last_arm: expr) => {
+        match INPUT[$idx + $off] {
+            d1 @ b'0'..=b'9' => match INPUT[$idx + $off + 1] {
+                d2 @ b'0'..=b'9' => match INPUT[$idx + $off + 2] {
+                    d3 @ b'0'..=b'9' => match INPUT[$idx + $off + 3] {
+                        $last_arm => (
+                            ((d1 - b'0') as u32 * 100)
+                                + ((d2 - b'0') as u32 * 10)
+                                + ((d3 - b'0') as u32),
+                            $off + 4,
+                        ),
+                        _ => continue,
+                    },
+
+                    $last_arm => (((d1 - b'0') as u32 * 10) + ((d2 - b'0') as u32), $off + 3),
+                    _ => continue,
+                },
+
+                $last_arm => ((d1 - b'0') as u32, $off + 2),
+                _ => continue,
+            },
+
+            _ => continue,
+        }
+    };
+}
+
+// Answer: 170807108
 fn part_1() -> u32 {
+    let finder = Finder::new(b"mul(");
     let mut sum = 0;
+
+    for candidate in finder.find_iter(INPUT) {
+        let (a, next_offset) = lazy_read_number!(candidate, 4, b',');
+        let (b, _) = lazy_read_number!(candidate, next_offset, b')');
+
+        sum += a * b;
+    }
 
     sum
 }

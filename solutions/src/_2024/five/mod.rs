cc3a1155f71e421bf5a3f3a1f83571b0dc891d77
thunder04 <70254851+thunder04@users.noreply.github.com>
2024-12-05 22:10:14 +0200

    Use bitmasks for 2024, day 5, part 1

--- solutions/src/_2024/five/mod.rs
+++ solutions/src/_2024/five/mod.rs
@@ -44,6 +44,7 @@ pub fn run() -> super::Runner {
 fn part_1() -> u32 {
     let mut ll = SpecializedLinkedList::<BIGGEST_NODE>::new();
     let mut input = INPUT;
+    let mut sum: u32 = 0;
 
     // Read all rules from input.
     #[allow(clippy::while_let_loop)]
@@ -60,14 +61,15 @@ fn part_1() -> u32 {
         debug_assert_ne!(node, 0); // The code doesn't expect 0 integers.
         debug_assert_ne!(rule, 0); // The code doesn't expect 0 integers.
 
-        ll.push_to(node as u8, rule as u8);
+        ll.add_rule(node as u8, rule as u8);
         input = &input[off_b + 1..];
     }
 
     let mut buf = Vec::with_capacity(32);
-    let mut sum: u32 = 0;
 
     loop {
+        let mut expected_rules = 0_u128;
+
         // Store comma separated numbers to `buf`.
         #[allow(clippy::while_let_loop)]
         loop {
@@ -77,6 +79,7 @@ fn part_1() -> u32 {
             let matched_char = input[off_a];
 
             input = &input[off_a + 1..];
+            expected_rules |= 1 << a;
             buf.push(a as u8);
 
             if matched_char == b'\n' {
@@ -84,31 +87,12 @@ fn part_1() -> u32 {
             }
         }
 
-        let mut is_valid = true;
+        if buf.iter().all(|page| {
+            // Remove the current page from the rules. A rule like "X | X" doesn't make sense.
+            expected_rules &= !(1 << page);
 
-        // debug!("LINE: {buf:?}");
-
-        // FIXME: Suboptimal O(n^3) loop. Express buf and rules as bitmasks and perform some bit
-        // magic for O(1). Will require u128s. Time was ~62μs on Default perf. profile.
-        'outer: for (idx, page) in buf.iter().enumerate() {
-            let rules = ll.rules_for(*page);
-
-            // debug!(
-            //     "RULES for {page}: {:?}. Will search: {:?}",
-            //     &(&*rules)[0..rules.len()],
-            //     &buf[idx + 1..]
-            // );
-
-            for page in &buf[idx + 1..] {
-                if !rules.contains(page) {
-                    is_valid = false;
-
-                    break 'outer;
-                }
-            }
-        }
-
-        if is_valid {
+            ll.contains_all_rules(*page, expected_rules)
+        }) {
             sum += buf[buf.len() / 2] as u32;
         }
 
@@ -128,65 +112,34 @@ fn part_2() -> u32 {
 }
 
 mod specialized_ll {
-    use std::ops::{Deref, Index};
-
-    use super::MAX_RULES_PER_NODE as RULES_SIZE;
-
     // TODO: After you are done with the day. Store [u8; MAX_RULES_PER_NODE] as a u128 bitmask,
     // since max node number ID can be 100.
-    pub struct SpecializedLinkedList<const SIZE: usize>([(usize, [u8; RULES_SIZE]); SIZE]);
+    pub struct SpecializedLinkedList<const SIZE: usize>([u128; SIZE]);
 
     impl<const SIZE: usize> SpecializedLinkedList<SIZE> {
-        pub const fn new() -> Self {
-            Self([(0, [0; RULES_SIZE]); SIZE])
-        }
-
-        /// Push a new rule for node.
-        pub const fn push_to(&mut self, node: u8, rule: u8) {
-            let (len, buf) = &mut self.0[node as usize];
-
-            buf[*len] = rule;
-            *len += 1;
-        }
-
-        /// Get all rules for a node.
-        pub const fn rules_for(&self, node: u8) -> RuleView {
-            let (len, buf) = &self.0[node as usize];
-
-            RuleView { buf, len: *len }
-        }
-    }
-
-    #[derive(Debug)]
-    pub struct RuleView<'b> {
-        buf: &'b [u8; RULES_SIZE],
-        len: usize,
-    }
-
-    impl RuleView<'_> {
-        pub const fn len(&self) -> usize {
-            self.len
-        }
-    }
-
-    impl Index<usize> for RuleView<'_> {
-        type Output = u8;
-
         #[inline(always)]
-        fn index(&self, index: usize) -> &Self::Output {
-            assert!(index < self.len);
-
-            // The elements are stored backwards.
-            // SAFETY: `index` will always be <= `self.1 - 1`.
-            unsafe { self.buf.get_unchecked(self.len - 1 - index) }
+        pub const fn new() -> Self {
+            Self([0; SIZE])
         }
-    }
 
-    impl Deref for RuleView<'_> {
-        type Target = [u8];
+        /// Add a new rule for node.
+        #[inline(always)]
+        pub const fn add_rule(&mut self, node: u8, rule: u8) {
+            assert!(rule < u128::BITS as u8);
 
-        fn deref(&self) -> &Self::Target {
-            self.buf
+            self.0[node as usize] |= 1 << rule;
+        }
+
+        /// Obtain all rules of a node.
+        #[inline(always)]
+        pub const fn rules_of(&self, node: u8) -> u128 {
+            self.0[node as usize]
+        }
+
+        /// Ensures a node contains all rules specified by the `rules` bitmask.
+        #[inline(always)]
+        pub const fn contains_all_rules(&self, node: u8, rules: u128) -> bool {
+            (self.rules_of(node) & rules) == rules
         }
     }
 
@@ -198,31 +151,15 @@ mod specialized_ll {
         fn basic_usage() {
             let mut ll = SpecializedLinkedList::<100>::new();
 
-            ll.push_to(75, 29);
-            ll.push_to(75, 53);
-            ll.push_to(75, 47);
-            ll.push_to(75, 61);
-            ll.push_to(75, 13);
+            ll.add_rule(75, 29);
+            ll.add_rule(75, 13);
+            ll.add_rule(75, 1); // Extra rule.
 
-            let rules = ll.rules_for(75);
-            assert_eq!(rules.len(), 5);
+            ll.add_rule(10, 29);
+            ll.add_rule(10, 13);
 
-            assert_eq!(rules[0], 13, "rules[0] == 13");
-            assert_eq!(rules[1], 61, "rules[1] == 61");
-            assert_eq!(rules[2], 47, "rules[2] == 47");
-            assert_eq!(rules[3], 53, "rules[3] == 53");
-            assert_eq!(rules[4], 29, "rules[4] == 29");
-
-            assert_eq!(ll.rules_for(0).len(), 0);
-        }
-
-        #[test]
-        #[should_panic]
-        fn improper_usage() {
-            let ll = SpecializedLinkedList::<100>::new();
-            let rules = ll.rules_for(75);
-
-            std::hint::black_box(rules[0]);
+            assert!(ll.contains_all_rules(75, ll.rules_of(10)));
+            assert!(!ll.contains_all_rules(10, ll.rules_of(75)));
         }
     }
 }

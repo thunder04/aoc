2dee559adce9471c4838d2d6803e4fca0a98bee8
thunder04 <70254851+thunder04@users.noreply.github.com>
2024-12-14 23:18:02 +0200

    Add multi threading

--- src/_2024/_14.rs
+++ src/_2024/_14.rs
@@ -1,13 +1,16 @@
 use std::{cmp::Ordering, sync::Arc};
 
-use ahash::{HashMap, HashMapExt};
-use image::{ImageBuffer, Rgb};
+use rayon::prelude::*;
+
+// Both constants must be divisible by 2. I want nice numbers.
+const MAX_SECONDS: u32 = 30_000_u32.next_power_of_two();
+const SECONDS_PER_SLAVE: u32 = 256_u32.next_power_of_two();
 
 const X: u32 = 101;
 const Y: u32 = 103;
 
-type Point = (u32, u32);
 type Velocity = (i32, i32);
+type Point = (u32, u32);
 
 pub fn part_1(input: &[u8]) -> i64 {
     let mut quadrants = [0_i64; 4];
@@ -43,32 +46,38 @@ pub fn part_2(input: &[u8]) -> i64 {
         Arc::new(buf) // Used for threading.
     };
 
-    for t in 0..9000 {
-        let mut buf = [0_u128; Y as usize];
+    (0..=MAX_SECONDS / SECONDS_PER_SLAVE)
+        .into_par_iter()
+        .find_map_first(|offset| {
+            (SECONDS_PER_SLAVE * offset..=SECONDS_PER_SLAVE * (offset + 1)).find_map(|t| {
+                let mut buf = [0_u128; Y as usize];
 
-        for &(p0, v) in robots.iter() {
-            let (x, y) = simulate(p0, v, t);
-            let x = x.clamp(0, X - 1); // The function might have a tiny small bug, which I CBA to fix.
-            let y = y.clamp(0, Y - 1); // The function might have a tiny small bug, which I CBA to fix.
+                for &(p0, v) in robots.iter() {
+                    let (x, y) = simulate(p0, v, t);
+                    // The function might have a tiny small bug, which I CBA to fix.
+                    let x = x.clamp(0, X - 1);
+                    let y = y.clamp(0, Y - 1);
 
-            buf[y as usize] |= 1 << x;
-        }
-
-        for mut row in buf {
-            while row != 0 {
-                row >>= row.trailing_zeros();
-                let n = row.trailing_ones();
-
-                if n > 25 {
-                    return t as i64;
+                    buf[y as usize] |= 1 << x;
                 }
 
-                row >>= n;
-            }
-        }
-    }
+                for mut row in buf {
+                    while row != 0 {
+                        row >>= row.trailing_zeros();
+                        let n = row.trailing_ones();
 
-    panic!("No solution found")
+                        if n > 25 {
+                            return Some(t as i64);
+                        }
+
+                        row >>= n;
+                    }
+                }
+
+                None
+            })
+        })
+        .expect("No solution found within reasonable time")
 }
 
 #[inline]
